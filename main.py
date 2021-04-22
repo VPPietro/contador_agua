7bef41f30072ea01121a2db3c206dcdd98f1c6f7
Pietro Paraventi Vanelli <pietroparaventivanelli@gmail.com>
2021-04-22 20:10:22 -0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from usuario import Usuario
 
-teste = Usuario('Pietro')
-print(teste.__dict__)
+opcao = input('1 - Registra novo usuário\n'
+              '2 - Entra com usuário existente\n>')
+
+if opcao == 1:
+    # como fazer novo registro infinita vezes
+    pass
+
+if opcao == 2:
+    # como chamar o usuario existente
+    pass
