48e11975ec9c3dda1facc33dc8087ad89390a429
pietro.vanelli <pietroparaventivanelli@gmail.com>
2021-08-16 09:17:53 -0300

    remover old

--- Contador de Agua/old/media.py
+++
@@ -1,9 +0,0 @@
-
-class Media:
-
-    def __init__(self, dicionario_copos, list_quantidade):
-        self.__dicionario = dicionario_copos
-        self.__quantidade = list_quantidade
-
-    def media(self):
-        return sum(self.__quantidade) / len(self.__dicionario)
